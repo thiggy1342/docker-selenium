0929d420e335809cce32f3d44d3c7bd0dea766d4
Adam <athigpen@rsglab.com>
2016-02-08 10:38:24 -0500

    adding nodechromefirefox

---
+++ NodeChromeFirefoxDebug/entry_point.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+export GEOMETRY="$SCREEN_WIDTH""x""$SCREEN_HEIGHT""x""$SCREEN_DEPTH"
+
+if [ ! -e /opt/selenium/generate_config ]; then
+  echo No Selenium Node configuration file, the node-base image is not intended to be run directly. 1>&2
+  exit 1
+fi
+
+if [ -z "$HUB_PORT_4444_TCP_ADDR" ]; then
+  echo Not linked with a running Hub container 1>&2
+  exit 1
+fi
+
+if [ -z "$MAX_INSTANCES" ]; then
+  MAX_INSTANCES=1
+fi
+
+if [ -z "$MAX_SESSIONS" ]; then
+  MAX_SESSIONS=1
+fi
+
+ROOT=/opt/selenium
+CONF=$ROOT/config.json
+
+$ROOT/generate_config>$CONF
+echo "starting selenium node with configuration:"
+cat $CONF
+
+function shutdown {
+  kill -s SIGTERM $NODE_PID
+  wait $NODE_PID
+}
+
+REMOTE_HOST_PARAM=""
+if [ ! -z "$REMOTE_HOST" ]; then
+  echo "REMOTE_HOST variable is set, appending -remoteHost"
+  REMOTE_HOST_PARAM="-remoteHost $REMOTE_HOST"
+fi
+
+if [ ! -z "$SE_OPTS" ]; then
+  echo "appending selenium options: ${SE_OPTS}"
+fi
+
+# TODO: Look into http://www.seleniumhq.org/docs/05_selenium_rc.jsp#browser-side-logs
+
+env | cut -f 1 -d "=" | sort > asroot
+  sudo -E -u seluser -i env | cut -f 1 -d "=" | sort > asseluser
+  sudo -E -i -u seluser \
+  $(for E in $(grep -vxFf asseluser asroot); do echo $E=$(eval echo \$$E); done) \
+  DISPLAY=$DISPLAY \
+  xvfb-run --server-args="$DISPLAY -screen 0 $GEOMETRY -ac +extension RANDR" \
+  java ${JAVA_OPTS} -jar /opt/selenium/selenium-server-standalone.jar \
+    -role node \
+    -hub http://$HUB_PORT_4444_TCP_ADDR:$HUB_PORT_4444_TCP_PORT/grid/register \
+    ${REMOTE_HOST_PARAM} \
+    -nodeConfig /opt/selenium/config.json \
+    ${SE_OPTS} &
+NODE_PID=$!
+
+trap shutdown SIGTERM SIGINT
+for i in $(seq 1 10)
+do
+  xdpyinfo -display $DISPLAY >/dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    break
+  fi
+  echo Waiting xvfb...
+  sleep 0.5
+done
+
+fluxbox -display $DISPLAY &
+
+x11vnc -forever -usepw -shared -rfbport 5900 -display $DISPLAY &
+
+wait $NODE_PID
